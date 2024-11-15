76b612002785ac5bc2b0892a69f032add41b7645
rhit-cerasoml <cerasoml@rose-hulman.edu>
2024-11-14 20:51:00 -0500

    Added run configurations to use java agent while building demo

--- src/com/jflow/agent/Agent.java
+++ src/com/jflow/agent/Agent.java
@@ -6,6 +6,6 @@ public class Agent {
 
     public static void premain(String agentArgs, Instrumentation inst){
 
-
+        System.out.println("Agent Done!");
     }
 }
--- src/com/jflow/test/DemoMain.java
+++ src/com/jflow/test/DemoMain.java
@@ -1,4 +1,7 @@
 package com.jflow.test;
 
 public class DemoMain {
+    public static void main(String[] args){
+
+    }
 }

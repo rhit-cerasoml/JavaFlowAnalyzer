2e995c0acbd4129840d65e86dca525d9066c02be
rhit-cerasoml <cerasoml@rose-hulman.edu>
2024-11-14 20:42:14 -0500

    add manifest to agent

--- src/com/jflow/agent/Agent.java
+++ src/com/jflow/agent/Agent.java
@@ -1,4 +1,11 @@
 package com.jflow.agent;
 
+import java.lang.instrument.Instrumentation;
+
 public class Agent {
+
+    public static void premain(String agentArgs, Instrumentation inst){
+
+
+    }
 }
